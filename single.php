b8e7e0a9ca0cb529bdc11e1aaad33569d0735084
stentibbing <sten.tibbing@gmail.com>
2021-12-09 15:05:02 +0200

    New single product view with quotation, accent color and multicolor title

--- single.php
+++ single.php
@@ -7,6 +7,26 @@
  * @package Deary
  */
 
+$post_meta = get_post_meta($post->ID);
+
+// Set accent color, use default black if not set
+if (array_key_exists('nm_accent_color', $post_meta) && !empty($post_meta['nm_accent_color'])) {
+    $accent_color = $post_meta['nm_accent_color'][0];
+} else {
+    $accent_color = '#000';
+}
+
+// Use multicolor title if set, regular title if not
+if (array_key_exists('nm_multicolor_title', $post_meta) && !empty($post_meta['nm_multicolor_title'])) {
+    $multicolor_title = $post_meta['nm_multicolor_title'][0];
+    $title = preg_replace_callback('/%(.*?)%/s', function ($matches) use ($accent_color) {
+        return '<span style="color:' . $accent_color . '">' . $matches[1] . '</span>';
+    }, $multicolor_title);
+} else {
+    $title = $post->post_title;
+}
+
+
 get_header();
 ?>
 
@@ -14,37 +34,36 @@ get_header();
 		
 		<article class="nm-product">
 			<div class="nm-product-top">
-				<div class="nm-product-title">
-					<h1><?php echo $post->post_title;?></h1>
-				</div>
-				<div class="nm-product-content">
-					<p><?php echo $post->post_content;?></p>
-				</div>
-			</div>
-			<div class="nm-product-bottom">
-				<div class="nm-product-image">
-						<?php echo get_the_post_thumbnail($post); ?>
-				</div>
-				
-					
-				<?php if (isset(get_post_meta($post->ID)['nm_nutrition_facts']) && !empty(get_post_meta($post->ID)['nm_nutrition_facts'][0])): ?>
-					
-					<div class="nm-product-nutrition-facts">
-					<?php
-					foreach(get_post_meta($post->ID)['nm_nutrition_facts'] as $post_nutrition_facts) {
-						echo $post_nutrition_facts;
-					}
-					?>
+				<div class="nm-product-top-left">
+					<div class="nm-product-title">
+						<h1><?php echo $title;?></h1>
 					</div>
-				<?php endif; ?>
-					
-				
+					<div class="nm-product-content">
+						<?php echo wpautop($post->post_content);?>
+					</div>
+				</div>
+				<div class="nm-product-top-center">
+					<div class="nm-product-image">
+							<?php echo get_the_post_thumbnail($post); ?>
+					</div>
+				</div>
+				<div class="nm-product-top-right">
+					<?php if (array_key_exists('nm_quote', $post_meta)): ?>
+						<div class="nm-product-quote">
+							<p style="color:<?php echo $accent_color; ?>"><?php echo "\"" . $post_meta['nm_quote'][0] . "\""; ?></p>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>	
+			<?php if (array_key_exists('nm_nutrition_facts', $post_meta)): ?>
+			<div class="nm-product-bottom">
+				<div class="nm-product-nutrition-facts">
+					<?php echo wpautop($post_meta['nm_nutrition_facts'][0]); ?>
+				</div>
 			</div>
-
+			<?php endif; ?>							
 		</article>
-
 	</main><!-- #main -->
 
 <?php
 get_footer();
-
